c9596090c5bfef6cb9b76acc53a052c419403251
Jean-Paul R. Soucy <31170087+jeanpaulrsoucy@users.noreply.github.com>
2020-01-15 10:02:41 -0500

    Updates visualization script.
    - Adds plot for a specific states
    - Adds centred title to plot
    - Makes saving plots optional

--- lyme_usa_1993-2009.R
+++ lyme_usa_1993-2009.R
@@ -18,21 +18,34 @@ lyme_state <- as_tsibble(lyme_state, key = state, index = year)
 national_plot <- ggplot(data = lyme_national, aes(x = year, y = incidence)) +
   geom_line() +
   theme_classic() +
-  labs(x = "Year", y = "Incidence/1,000")
+  theme(plot.title = element_text(hjust = 0.5)) + # centre title
+  labs(x = "Year", y = "Incidence/1,000", title = "USA - National")
 national_plot # display plot
-ggsave("national_plot.png", national_plot) # save plot to working directory
+# ggsave("national_plot.png", national_plot) # save plot to working directory
 
 # Plot - USA states
 state_plot <- ggplot(data = lyme_state, aes(x = year, y = incidence.per.1000, group = state)) +
   geom_line() +
   theme_classic() +
-  labs(x = "Year", y = "Incidence/1,000")
+  theme(plot.title = element_text(hjust = 0.5)) + # centre title
+  labs(x = "Year", y = "Incidence/1,000", title = "USA - 50 States and DC")
 state_plot # display plot
-ggsave("state_plot.png", state_plot) # save plot to working directory
+# ggsave("state_plot.png", state_plot) # save plot to working directory
 
 # Add country-level trend to state plot
 state_plot_national <- state_plot +
   geom_line(data = lyme_national, aes(x = year, y = incidence, group = 1),
                                  colour = "blue", size = 3, alpha = 0.5)
 state_plot_national # display plot
-ggsave("state_plot_national.png", state_plot_national) # save plot to working directory
+# ggsave("state_plot_national.png", state_plot_national) # save plot to working directory
+
+# Plot - specific state
+state <- "Connecticut" # define state of interest
+specific_state_plot <- ggplot(data = lyme_state[lyme_state$state == state, ],
+                              aes(x = year, y = incidence.per.1000)) +
+  geom_line() +
+  theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5)) + # centre title
+  labs(x = "Year", y = "Incidence/1,000", title = state)
+specific_state_plot # display plot
+# ggsave("specific_state_plot.png", specific_state_plot) # save plot to working directory
